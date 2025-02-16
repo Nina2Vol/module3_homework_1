946dbfad39ba0bcbc08f2e35cf7d90f945eef347
Nina2Vol <nezlob2003@gmail.com>
2025-02-17 01:51:34 +0300

    Homework1
    1-3 задания

---
+++ homework1.js
@@ -0,0 +1,19 @@
+
+// задача 1 //
+let str = 'abcde';
+alert(str[0]);
+lert(str[1]);
+alert(str[4]);
+
+// задача 2 //
+let a = parseInt(prompt("Введите первое число:"));
+let b = parseInt(prompt("Введите второе число:"));
+console.log(a > 0 || b > 0);
+    
+// задача 3 //
+let age = parseInt(prompt("Введите ваш возраст:"));
+if (age >= 18) {
+    console.log("Совершеннолетний");
+} else {
+    console.log("Несовершеннолетний");
+}
